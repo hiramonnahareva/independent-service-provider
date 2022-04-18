c0def7423258ab130973cd85a2b52899b326562d
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-04-18 20:24:43 +0600

    update checkout page

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
+import 'reactjs-popup/dist/index.css';
 const Checkout = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const handleEmail = event => {
         setEmail(event.target.value) ;
+        console.log (event.target.value)
        }
        const handlePassword = event => {
            setPassword (event.target.value) ;
        }
        const handleFormSubmit = event => {
-        event.preventDefault();
-        if (email && password) {
-
-        }
+           if (email && password){
+            alert ('Hello !! welcome to this website')
+           }
+           event.preventDefault()
        }
     return (
         
@@ -22,17 +24,12 @@ const Checkout = () => {
             <h2>Checkout</h2>
                 <form onSubmit={handleFormSubmit}>
                     <input  className='input-field' type="text" placeholder='Enter Your Name'/>
+                    <input  className='input-field' type="text" placeholder='Enter Your Address'/>
+                    <input  className='input-field' type="text" placeholder='Enter Your Phone number'/>
                     <input onBlur={handleEmail} className='input-field' type="email" placeholder='Enter Your Email' required/>
                     <input  onBlur={handlePassword} className='input-field' type="password" placeholder='Enter Your Password' required/>
                     <input className='submit-btn' type="submit" value="search" />
                 </form>
-        <div className='or-part'>
-            <hr className='hr-line' />
-           <span> OR </span>
-            <hr className='hr-line' />
-            
-        </div>
-    
             </div>
         </div>
         </div>
